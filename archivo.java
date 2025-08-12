a9a15e8dc92c88d1ba6690d57e06a5a1666a9999
406 - Comunicaciones <406@laboratorios.usbbog.edu.co>
2025-08-11 19:02:40 -0500

    feature: no funciona

--- archivo.java
+++ archivo.java
@@ -1,8 +1,9 @@
+//mmmm como que esta tampoco 
 public void main(){
     system.out.println("hello world");
 }
 
-
+//mmm como que no funciona esta 
 public void saludo(){
     return "hola,como estas";
 }
