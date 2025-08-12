fb62a67e742fd163cb88a7deb840a4651f08b2da
406 - Comunicaciones <406@laboratorios.usbbog.edu.co>
2025-08-11 19:29:42 -0500

    Release: archivo completado

--- archivo.java
+++ archivo.java
@@ -1,9 +1,9 @@
-
+// gracias desarrollador te quiero mucho 
 public void main(){
     system.out.println("hola mundo");
 }
 
-
+// funciona para mi 
 public void saludo(){
     return "hi,how are you";
 }
